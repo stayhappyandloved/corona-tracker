744b24d8a18b137655909e89aaa687d56e759377
stayhappyandloved <theajxz14@gmail.com>
2021-02-04 22:47:20 +0800

    finished project

--- src/App.js
+++ src/App.js
@@ -1,30 +1,41 @@
 import React from 'react';
-import {Cards, Chart, CountryPicker} from './components';
+
+import { Cards, CountryPicker, Chart } from './components';
+import { fetchData } from './api/';
 import styles from './App.module.css';
-import {fetchData} from './api'; //it would automatically run index.js
 
+import image from './images/image.png';
 
-class App extends React.Component{
+class App extends React.Component {
+  state = {
+    data: {},
+    country: '',
+  }
 
-    state = {
-        data: {},
-    }
+  async componentDidMount() {
+    const data = await fetchData();
 
+    this.setState({ data });
+  }
 
-    async componentDidMount(){//best way to fetch data
-        const fetchedData = await fetchData();// await data needs to be fetched by an await func so you add asyn in from of component did mount
-        this.setState({data:fetchedData});
-    }
-    render(){
-        const {data} = this.state;
-        return (
-            <div className={styles.container}>
-               <Cards  data={data}/>
-               <CountryPicker />
-               <Chart />
-            </div>
-        )
-    }
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  }
+
+  render() {
+    const { data, country } = this.state;
+
+    return (
+      <div className={styles.container}>
+        <img className={styles.image} src={image} alt="COVID-19" />
+        <Cards data={data} />
+        <CountryPicker handleCountryChange={this.handleCountryChange} />
+        <Chart data={data} country={country} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,24 +1,41 @@
 import React from 'react';
-import {Card, CardContent, Typography, Grid} from '@material-ui/core';
+import { Typography, Grid } from '@material-ui/core';
+import CardComponent from './Card/Card';
 import styles from './Cards.module.css';
 
-const Cards = (props) =>{
+const Info = ({ data: { confirmed, recovered, deaths, lastUpdate } }) => {
+  if (!confirmed) {
+    return 'Loading...';
+  }
 
-    console.log(props);
-    return (
-        <div classname={styles.container}>
-            <Grid container spacing={3} justify="content">
-                <Grid item component ={Card}>
-                    <CardContent>
-                        <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">Real data</Typography>
-                        <Typography color="textSecondary">Real Date</Typography>
-                        <Typography variant="body2">Number of active cases of COVID-19</Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </div>
-    )
-}
+  return (
+    <div className={styles.container}>
+        <Typography gutterBottom variant="h4" component="h2">Global</Typography>
+      <Grid container spacing={3} justify="center">
+        <CardComponent
+          className={styles.infected}
+          cardTitle="Infected"
+          value={confirmed.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of active cases from COVID-19."
+        />
+        <CardComponent
+          className={styles.recovered}
+          cardTitle="Recovered"
+          value={recovered.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of recoveries from COVID-19."
+        />
+        <CardComponent
+          className={styles.deaths}
+          cardTitle="Deaths"
+          value={deaths.value}
+          lastUpdate={lastUpdate}
+          cardSubtitle="Number of deaths caused by COVID-19."
+        />
+      </Grid>
+    </div>
+  );
+};
 
-export default Cards;
+export default Info;
