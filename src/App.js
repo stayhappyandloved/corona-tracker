4d337d9d980a1892ca98ac68115fe33539d3128a
stayhappyandloved <theajxz14@gmail.com>
2021-02-04 20:03:34 +0800

    fetch data from componentDidMount and render it

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import {fetchData} from './api'; //it would automatically run index.js
 
 
 class App extends React.Component{
+
+    state = {
+        data: {},
+    }
+
+
     async componentDidMount(){//best way to fetch data
-        const data = await fetchData();// await data needs to be fetched by an await func so you add asyn in from of component did mount
-        console.log(data);
+        const fetchedData = await fetchData();// await data needs to be fetched by an await func so you add asyn in from of component did mount
+        this.setState({data:fetchedData});
     }
     render(){
+        const {data} = this.state;
         return (
             <div className={styles.container}>
-               <Cards />
+               <Cards  data={data}/>
                <CountryPicker />
                <Chart />
             </div>
--- src/api/index.js
+++ src/api/index.js
@@ -5,15 +5,10 @@ const url = 'https://api.covidtracking.com/v1/us/daily.json​';
 //have an async function built on promises
 export const fetchData = async () => {
     try{
-        const {data} = await axios.get(url);
+        const {data: {confirmed, recovered, deaths, lastUpdate}} = await axios.get(url);
 
-        const modifiedData = {
-            confirmed: data.confirmed,
-            recovered: data.recovered,
-            deaths: data.deaths,
-            lastUpdate: data.lastUpdate,
-        }
-        return modifiedData;
+       
+        return {confirmed, recovered, deaths, lastUpdate};
     } catch(error){
        
     }
